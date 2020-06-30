faea7b21ae0025a80b23ccf37361434526049acb
amitkumarkabbur <67629640+amitkumarkabbur@users.noreply.github.com>
2020-06-30 18:18:45 +0530

    Update Product.cs

--- Product.cs
+++ Product.cs
@@ -2,6 +2,13 @@ namespace PromotionEngine
 {
   public class Product 
   {
-
+    public string Name {get; set;}
+    public decimal Price { get; set;}
+    
+    public Product (string name, decimal price)
+    {
+      Name = name;
+      Price = price;
+    }
   }
 }
