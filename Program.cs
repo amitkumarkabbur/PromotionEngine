05c883a98e7e3236f5139b58aedef5014602fb45
amitkumarkabbur <67629640+amitkumarkabbur@users.noreply.github.com>
2020-06-30 20:56:39 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,11 +6,27 @@ namespace PromotionEngine
      {
      
       #region "Scenario A"
-            Order scenarioA = new Order(1, new List<Product>() { new Product("A", 1), new Product("B", 1), new Product("C", 1) });
+            Order scenarioA = new Order(1, new List<Product>() { new Product("A", 50.0), new Product("B", 30.0), new Product("C", 20.0) });
             decimal finalPrice = PromotionBusinessRules.GetFinalPrice(scenarioA);
       #endregion
       
+        #region "Scenario B"
+        Order scenarioB = new Order(1, new List<Product>() { new Product("A", 50.0), new Product("A", 50.0), new Product("A", 50.0),
+                                                          new Product("A", 50.0), new Product("A", 50.0), new Product("B", 30.0),
+                                                          new Product("B", 30.0), new Product("B", 30.0), new Product("B", 30.0),
+                                                          new Product("B", 30.0), new Product("C", 20.0)
+                                                          });
+        decimal finalPrice2 = PromotionBusinessRules.GetFinalPrice(scenarioB);
+        #endregion
       
+        #region "Scenario C"
+        Order scenarioC = new Order(1, new List<Product>() { new Product("A", 50.0), new Product("A", 50.0), new Product("A", 50.0),
+                                                          new Product("B", 30.0), new Product("B", 30.0), new Product("B", 30.0),
+                                                          new Product("B", 30.0), new Product("B", 30.0), new Product("C", 20.0),
+                                                          new Product("D", 15.0)
+                                                          });
+        decimal finalPrice3 = PromotionBusinessRules.GetFinalPrice(scenarioC);
+        #endregion
       
      }
      
